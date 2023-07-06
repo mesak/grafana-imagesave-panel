a781e01c0d9397a358c4951b126e049926802e52
Mesak <mesakey@gmail.com>
2023-07-07 03:27:16 +0800

    release: community signature

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 rm -f dist release/mesak-imagesave-panel.zip release/mesak-imagesave-panel.sha1
 yarn build
-npx @grafana/sign-plugin@latest --rootUrls https://**.grafana.com
+npx @grafana/sign-plugin@latest 
 mv dist mesak-imagesave-panel
 zip -r release/mesak-imagesave-panel.zip mesak-imagesave-panel
 mv mesak-imagesave-panel dist
